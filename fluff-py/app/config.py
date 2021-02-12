7ad4387059bfaa6a237763d0d73d999d49f41b3a
NoSoundLeR <slowercod7bo@gmail.com>
2021-02-12 16:25:28 +0300

    length and new length

--- fluff-py/app/config.py
+++ fluff-py/app/config.py
@@ -5,5 +5,6 @@ DEBUG = os.environ.get("DEBUG") in ["1", "true", "True"]
 
 REDIS_URL = os.environ.get("REDIS_URL") or "redis://localhost/"
 
-LINK_LENGTH = 8
+LINK_LENGTH = 16
+LINK_NEW_LENGTH = 6
 LINK_CHARS = "".join([ascii_letters, digits])
--- fluff-py/app/keys.py
+++ fluff-py/app/keys.py
@@ -1,7 +1,7 @@
 import random
 
-from app.config import LINK_CHARS, LINK_LENGTH
+from app.config import LINK_CHARS, LINK_NEW_LENGTH
 
 
-def create_random_key():
-    return "".join(random.choices(LINK_CHARS, k=LINK_LENGTH))
+def create_random_key() -> str:
+    return "".join(random.choices(LINK_CHARS, k=LINK_NEW_LENGTH))
