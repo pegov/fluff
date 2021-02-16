e2903897d71e552e0ec95318ed3d540ba9ff50f1
NoSoundLeR <slowercod7bo@gmail.com>
2021-02-16 20:52:16 +0300

    Docker py

--- fluff-py/app/config.py
+++ fluff-py/app/config.py
@@ -3,7 +3,7 @@ from string import ascii_letters, digits
 
 DEBUG = os.environ.get("DEBUG") in ["1", "true", "True"]
 
-REDIS_URL = os.environ.get("REDIS_URL") or "redis://localhost/"
+REDIS_URL = os.getenv("REDIS_URL") or "redis://localhost/"
 
 LINK_MIN_LENGTH = 2
 LINK_MAX_LENGTH = 32
